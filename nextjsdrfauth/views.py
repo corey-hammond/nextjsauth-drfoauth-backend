825abe3c0895ba347f6f7aff8fafc8b9fa881f47
Corey Hammond <coreyjhammond@gmail.com>
2021-03-29 16:49:52 -0700

    created google login view

--- nextjsdrfauth/views.py
+++ nextjsdrfauth/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.conf import settings
+from dj_rest_auth.registration.views import SocialLoginView
+from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
+from allauth.socialaccount.providers.oauth2.client import OAuth2Client
 
-# Create your views here.
+class GoogleLoginView(SocialLoginView):
+  authentication_classes = [] # disable authentication, make sure to override 'allowed origins' in settings.py in production!
+  adapter_class = GoogleOAuth2Adapter
+  callback_url = 'http://localhost:3000'
+  client_class = OAuth2Client
